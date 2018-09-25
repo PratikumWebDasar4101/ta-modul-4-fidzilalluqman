9c1b59bc3789c9b573dd0b73fb8404c34d476cb7
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-09-25 19:20:31 +0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,3 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1 align="center">Login</h1>
+<table border="1" align="center">
+	<form action="login.php" method="POST">
+	<tr>
+		<th>Username</th>
+		<td><input type="text" name="username" id="username"></td>
+	</tr>
+	<tr>
+		<th>Password</th>
+		<td><input type="password" name="password" id="password"></td>
+	</tr>
+	<tr>
+		<td colspan="2"><input style="width: 100%" type="submit" name="submit"></td>
+	</tr>
+</form> 
+</table>
+</body>
+</html>
 
+<?php  
+session_start();
+error_reporting(0);
+$user = array(
+				"username" => "admin",
+				"password" => "admin"
+			);
+if (isset($_POST['submit'])) {
+	if ($_POST['username'] == $user['username'] && $_POST['password'] == $user['password']){
+		$_SESSION['username'] = $_POST['username'];
+		header("Location: formawal.html");
+	}else{
+		echo "Username atau Password Tidak Benar";
+	}
+}
 ?>
